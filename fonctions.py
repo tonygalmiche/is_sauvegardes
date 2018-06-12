780868492b43128f2d9d4f1c83706c54a1c21ef0
Tony Galmiche <tony.galmiche@infosaone.com>
2018-06-12 16:35:27 +0200

    bug avec les accents

--- fonctions.py
+++ fonctions.py
@@ -1,10 +1,17 @@
 # -*- coding: utf-8 -*-
 import psycopg2
 import psycopg2.extras
+import unicodedata
 
 def s(txt,lg):
     #print txt,lg,type(txt)
-    return (unicode(txt, 'utf-8')+u'                                                             ')[:lg]
+
+    txt = unicode(txt,'utf-8')
+    txt = unicodedata.normalize('NFD', txt).encode('ascii', 'ignore')
+    txt = (txt+u'                                                             ')[:lg]
+    return txt
+
+    #return (unicode(txt, 'utf-8')+u'                                                             ')[:lg]
 
 
 def log(msg):
